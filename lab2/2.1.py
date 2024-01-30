42100177d81c5d269dfebdb46a2a0fa8ae9f3c3f
zhuldyzsagidulla <sagidulla_j@icloud.com>
2024-01-30 11:47:36 +0600

    lab2 in progress

---
+++ lab2/2.1.py
@@ -0,0 +1,25 @@
+#Booleans 1
+
+print(10>9)
+ True 
+#Booleans 2
+ 
+print(10==9)
+  False
+
+#Booleans 3
+
+print(10<9)
+ False
+
+#Booleans 4
+
+print(bool("abc"))
+ True
+
+#Booleans 5
+
+print(bool(0))
+ False
+
+ 
---
+++ lab2/2.2.py
@@ -0,0 +1,24 @@
+#Operators 1
+
+print(10*5)
+
+#Operators 2
+
+print(10/2)
+
+#Operators 3
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+#Operators 4
+
+if 5!=10:
+  print("5 and 10 is not equal")
+
+#Operators 5
+
+if 5==10 or 4==4:
+    print("At least one of the statements is true")
+ 
